e077db8e9bb2235b5298674524442623f2cd35fb
Jonathan Luk <jluk87@gmail.com>
2019-08-10 20:29:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ class BoardMember () {
   }
 
   veto () {
-    return "No, I must disagree"
+    return "No, I must disagree";
   }
 
   approve () {
-    return "You can do that"
+    return "You can do that";
   }
 
   doCharity () {
-    return "I like to help people."
+    return "I like to help people.";
   }
 
   releasePressStatement () {
-    return "You will see great things from Scuber."
+    return "You will see great things from Scuber.";
   }
 
   sayHi () {
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;
   }
 
 }
