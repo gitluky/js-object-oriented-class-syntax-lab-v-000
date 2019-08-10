08aec32df341bf2f32198c0607ca88bdd34eb110
Jonathan Luk <jluk87@gmail.com>
2019-08-10 20:27:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+class BoardMember () {
+  constructor () {
+    this.name = name;
+    this.homestate = homestate;
+    this.training = training;
+  }
+
+  veto () {
+    return "No, I must disagree"
+  }
+
+  approve () {
+    return "You can do that"
+  }
+
+  doCharity () {
+    return "I like to help people."
+  }
+
+  releasePressStatement () {
+    return "You will see great things from Scuber."
+  }
+
+  sayHi () {
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+  }
+
+
+}
